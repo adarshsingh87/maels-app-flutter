c568533e384479f8fe3f3dcf0560c06b5ec33bec
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-12-10 22:55:21 +0530

    started a new meals app to learn about multiple screens

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/category_item.dart';
+
+import '../dummy_data.dart';
+
+class CategoriesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(20),
+        children: DUMMY_CATEGORIES
+            .map((e) => CategoryItem(e.title, e.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+      ),
+    );
+  }
+}
