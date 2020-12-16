200a139e1b99a51165d95e0cc438ee6fb97ab41a
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2020-12-16 23:51:13 +0530

    added meals

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   final String categoryTitle;
   final String categoryId;
-  CategoryMealsScreen( this.categoryTitle, this.categoryId);
+  CategoryMealsScreen(this.categoryTitle, this.categoryId);
 
   @override
   Widget build(BuildContext context) {
+    final categoryMeals = DUMMY_MEALS.where((e) {
+      return e.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text('new screen id: ${categoryId} poggers'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealsItem(
+              imageUrl: categoryMeals[index].imageUrl,
+              title: categoryMeals[index].title,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration);
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
